b1be3ed2e62aa9a3816a53db4dc16b73286188b7
Rahul D Shetty <rdshetty@vmware.com>
2020-05-29 10:33:10 +0530

    updated to show mean time and other params

--- main-keras.py
+++ main-keras.py
@@ -2,18 +2,17 @@ import horovod.tensorflow.keras as hvd
 import numpy as np 
 import h5py
 import tensorflow as tf
+import tensorflow.keras.backend as K
 from lib import cnn
+from time import time
 
 # Initialize Horovod
 hvd.init()
 
 # Pin GPU to be used to process local rank (one GPU per process)
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-if gpus:
-    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
-
+config = tf.ConfigProto()
+config.gpu_options.visible_device_list = str(hvd.local_rank())
+K.set_session(tf.Session(config=config))
 
 def loadDataH5(): 
     with h5py.File('./files/data1.h5','r') as hf: 
@@ -25,10 +24,26 @@ def loadDataH5():
         print (valX.shape,valY.shape) 
     return trainX, trainY, valX, valY 
 
+class TimingCallback(tf.keras.callbacks.Callback):
+    def __init__():
+        self.logs=[]
+  
+    def on_epoch_begin(epoch, logs={}):
+        self.starttime=time()
+  
+    def on_epoch_end(epoch, logs={}):
+        time_interval = time()-self.starttime
+        print("Time taken for epoch {} : {}".format(epoch, time_interval))
+        self.logs.append(time_interval)
+
 if __name__ == '__main__':
     trainX, trainY, testX, testY = loadDataH5()
     
     NUM_EPOCHS =50
+
+    # Horovod: adjust number of epochs based on number of Processing units.
+    epochs = int(math.ceil(NUM_EPOCHS / hvd.size()))
+
     inshape=trainX.shape[1:]
     classes=np.unique(trainY).size
     
@@ -49,6 +64,8 @@ if __name__ == '__main__':
         # This is necessary to ensure consistent initialization of all workers when
         # training is started with random weights or restored from a checkpoint.
         hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+        
+        TimingCallback()
     ]
 
     # Horovod: save checkpoints only on worker 0 to prevent other workers from corrupting them.
@@ -58,6 +75,12 @@ if __name__ == '__main__':
 
     print (model.summary())
     print("Training network...")
-    History = model.fit(trainX, trainY, batch_size=16, 
-                epochs=NUM_EPOCHS, validation_data=(testX, testY), callbacks=callbacks, verbose=1 if hvd.rank() == 0 else 0)
+    History = model.fit(
+        trainX, 
+        trainY,
+        batch_size=16, 
+        epochs=epochs, 
+        validation_data=(testX, testY), 
+        callbacks=callbacks, 
+        verbose=1 if hvd.rank() == 0 else 0)
     print ("Test Data Loss and Accuracy: ", model.evaluate(testX, testY))
